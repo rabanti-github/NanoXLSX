993fe3b7ec6acc08e74602babc8d1b27eab357f7
Raphael Stoeckli <github@rabanti.ch>
2022-03-29 23:22:12 +0200

    Adapted border style tests

--- NanoXlsx Test/Styles/WriteRead/BorderWriteReadTest.cs
+++ NanoXlsx Test/Styles/WriteRead/BorderWriteReadTest.cs
@@ -13,7 +13,7 @@ namespace NanoXLSX_Test.Styles.WriteRead
     public class BorderWriteReadTest
     {
 
-        private enum BorderDirection
+        public enum BorderDirection
         {
             Diagonal,
             Left,
@@ -27,6 +27,7 @@ namespace NanoXLSX_Test.Styles.WriteRead
         [InlineData("FFAADD00", 0.5f, true, false)]
         [InlineData("FFDDCC00", true, false, true)]
         [InlineData("FFAACCDD", null, false, false)]
+        [InlineData(null, 22, true, true)]
         public void DiagonalColorTest(String color, object value, bool diagonalUp, bool diagonalDown)
         {
             Style style = new Style();
@@ -48,6 +49,7 @@ namespace NanoXLSX_Test.Styles.WriteRead
         [InlineData("FFAADD00", 0.5f)]
         [InlineData("FFDDCC00", true)]
         [InlineData("FFAACCDD", null)]
+        [InlineData(null, 22)]
         public void TopColorTest(String color, object value)
         {
             Style style = new Style();
@@ -66,6 +68,7 @@ namespace NanoXLSX_Test.Styles.WriteRead
         [InlineData("FFAADD00", 0.5f)]
         [InlineData("FFDDCC00", true)]
         [InlineData("FFAACCDD", null)]
+        [InlineData(null, 22)]
         public void BottomColorTest(String color, object value)
         {
             Style style = new Style();
@@ -83,6 +86,7 @@ namespace NanoXLSX_Test.Styles.WriteRead
         [InlineData("FFAADD00", 0.5f)]
         [InlineData("FFDDCC00", true)]
         [InlineData("FFAACCDD", null)]
+        [InlineData(null, 22)]
         public void LeftColorTest(String color, object value)
         {
             Style style = new Style();
@@ -100,6 +104,7 @@ namespace NanoXLSX_Test.Styles.WriteRead
         [InlineData("FFAADD00", 0.5f)]
         [InlineData("FFDDCC00", true)]
         [InlineData("FFAACCDD", null)]
+        [InlineData(null, 22)]
         public void RightColorTest(String color, object value)
         {
             Style style = new Style();
@@ -113,28 +118,60 @@ namespace NanoXLSX_Test.Styles.WriteRead
         }
 
         [Theory(DisplayName = "Test of the writing and reading of border style value")]
-        [InlineData(Border.StyleValue.dashDotDot)]
-        [InlineData(Border.StyleValue.dashDot)]
-        [InlineData(Border.StyleValue.dashed)]
-        [InlineData(Border.StyleValue.dotted)]
-        [InlineData(Border.StyleValue.hair)]
-        [InlineData(Border.StyleValue.medium)]
-        [InlineData(Border.StyleValue.mediumDashDot)]
-        [InlineData(Border.StyleValue.mediumDashDotDot)]
-        [InlineData(Border.StyleValue.mediumDashed)]
-        [InlineData(Border.StyleValue.slantDashDot)]
-        [InlineData(Border.StyleValue.thin)]
-        [InlineData(Border.StyleValue.s_double)]
-        [InlineData(Border.StyleValue.thick)]
-        [InlineData(Border.StyleValue.none)]
-        public void BorderStyleTest(Border.StyleValue styleValue)
+        [InlineData(Border.StyleValue.dashDotDot, BorderDirection.Bottom)]
+        [InlineData(Border.StyleValue.dashDot, BorderDirection.Top)]
+        [InlineData(Border.StyleValue.dashed, BorderDirection.Left)]
+        [InlineData(Border.StyleValue.dotted, BorderDirection.Right)]
+        [InlineData(Border.StyleValue.hair, BorderDirection.Diagonal)]
+        [InlineData(Border.StyleValue.medium, BorderDirection.Bottom)]
+        [InlineData(Border.StyleValue.mediumDashDot, BorderDirection.Top)]
+        [InlineData(Border.StyleValue.mediumDashDotDot, BorderDirection.Left)]
+        [InlineData(Border.StyleValue.mediumDashed, BorderDirection.Right)]
+        [InlineData(Border.StyleValue.slantDashDot, BorderDirection.Diagonal)]
+        [InlineData(Border.StyleValue.thin, BorderDirection.Bottom)]
+        [InlineData(Border.StyleValue.s_double, BorderDirection.Top)]
+        [InlineData(Border.StyleValue.thick, BorderDirection.Left)]
+        [InlineData(Border.StyleValue.none, BorderDirection.Right)]
+        public void BorderStyleTest(Border.StyleValue styleValue, BorderDirection direction)
         {
             Style style = new Style();
-            style.CurrentBorder.RightStyle = styleValue;
-
+            switch (direction)
+            {
+                case BorderDirection.Diagonal:
+                    style.CurrentBorder.DiagonalStyle = styleValue;
+                    break;
+                case BorderDirection.Left:
+                    style.CurrentBorder.LeftStyle = styleValue;
+                    break;
+                case BorderDirection.Right:
+                    style.CurrentBorder.RightStyle = styleValue;
+                    break;
+                case BorderDirection.Top:
+                    style.CurrentBorder.TopStyle = styleValue;
+                    break;
+                case BorderDirection.Bottom:
+                    style.CurrentBorder.BottomStyle = styleValue;
+                    break;
+            }
             Cell cell = CreateWorkbook("test", style);
-
-            Assert.Equal(styleValue, cell.CellStyle.CurrentBorder.RightStyle);
+            switch (direction)
+            {
+                case BorderDirection.Diagonal:
+                    Assert.Equal(styleValue, cell.CellStyle.CurrentBorder.DiagonalStyle);
+                    break;
+                case BorderDirection.Left:
+                    Assert.Equal(styleValue, cell.CellStyle.CurrentBorder.LeftStyle);
+                    break;
+                case BorderDirection.Right:
+                    Assert.Equal(styleValue, cell.CellStyle.CurrentBorder.RightStyle);
+                    break;
+                case BorderDirection.Top:
+                    Assert.Equal(styleValue, cell.CellStyle.CurrentBorder.TopStyle);
+                    break;
+                case BorderDirection.Bottom:
+                    Assert.Equal(styleValue, cell.CellStyle.CurrentBorder.BottomStyle);
+                    break;
+            }
         }
 
         private static Cell CreateWorkbook(object value, Style style)
