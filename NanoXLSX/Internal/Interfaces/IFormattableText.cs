0c759a392c7aa252b882dbeae3c7ed60271745b0
Raphael Stoeckli <github@rabanti.ch>
2022-10-07 03:30:18 +0200

    Setup v3.x

---
+++ NanoXLSX/Internal/Interfaces/IFormattableText.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * NanoXLSX is a small .NET library to generate and read XLSX (Microsoft Excel 2007 or newer) files in an easy and native way
+ * Copyright Raphael Stoeckli © 2022
+ * This library is licensed under the MIT License.
+ * You find a copy of the license in project folder or on: http://opensource.org/licenses/MIT
+ */
+
+using System;
+using System.Text;
+
+namespace NanoXLSX.Internal.Interfaces
+{
+    /// <summary>
+    /// Interface to represent complex text data that can be formatted somehow
+    /// </summary>
+    public interface IFormattableText
+    {
+
+        /// <summary>
+        /// Method to add the formatted text value, from the object of the class that has implemented the interface, to the passed string builder
+        /// </summary>
+        /// <param name="sb">String builder instance</param>
+        /// <remarks>The formatted value must be XLM-attribute-ready, means already escaped and containing the right 
+        /// XML tags to be enclosed by &larr;si&rarr;...&larr;/si&rarr within the shared stings document</remarks>
+        void AddFormattedValue(StringBuilder sb);
+
+        
+    }
+}
---
+++ NanoXLSX/Internal/ReaderUtils.cs
@@ -0,0 +1,124 @@
+﻿/*
+* NanoXLSX is a small .NET library to generate and read XLSX (Microsoft Excel 2007 or newer) files in an easy and native way
+* Copyright Raphael Stoeckli © 2022
+* This library is licensed under the MIT License.
+* You find a copy of the license in project folder or on: http://opensource.org/licenses/MIT
+*/
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Xml;
+
+namespace NanoXLSX.Internal
+{
+    /// <summary>
+    /// Static class with common util methods, used during reading XLSX files
+    /// </summary>
+    public static class ReaderUtils
+    {
+        /// <summary>
+        /// Gets the XML attribute of the passed XML node by its name
+        /// </summary>
+        /// <param name="node">XML node that contains the attribute</param>
+        /// <param name="targetName">Name of the target attribute</param>
+        /// <param name="fallbackValue">Optional fallback value if the attribute was not found. Default is null</param>
+        /// <returns>Attribute value as string or default value if not found (can be null)</returns>
+        public static string GetAttribute(XmlNode node, string targetName, string fallbackValue = null)
+        {
+            if (node.Attributes == null || node.Attributes.Count == 0)
+            {
+                return fallbackValue;
+            }
+
+            foreach (XmlAttribute attribute in node.Attributes)
+            {
+                if (attribute.Name == targetName)
+                {
+                    return attribute.Value;
+                }
+            }
+
+            return fallbackValue;
+        }
+
+        /// <summary>
+        /// Gets the XML attribute from a child node of the passed XML node by its name and the name of the child node.
+        /// This method simplifies the process of gathering one single child node attribute
+        /// </summary>
+        /// <param name="node">XML node that contains the child node</param>
+        /// <param name="childNodeName">Name of the child node</param>
+        /// <param name="attributeName">Name of the attribute in the child node</param>
+        /// <param name="output">Value of the attribute as string or null if not found</param>
+        /// <returns>True if found, otherwise false</returns>
+        public static bool GetAttributeOfChild(XmlNode node, string childNodeName, string attributeName, out string output)
+        {
+            XmlNode childNode = GetChildNode(node, childNodeName);
+            if (childNode != null)
+            {
+                output = GetAttribute(childNode, attributeName);
+                return true;
+            }
+            output = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the specified child node
+        /// </summary>
+        /// <param name="node">XML node that contains child node</param>
+        /// <param name="name">Name of the child node</param>
+        /// <returns>Child node or null if not found</returns>
+        public static XmlNode GetChildNode(XmlNode node, string name)
+        {
+            if (node != null && node.HasChildNodes)
+            {
+                return node.ChildNodes.Cast<XmlNode>().FirstOrDefault(c => c.LocalName.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Checks whether the given node has the specified name
+        /// </summary>
+        /// <param name="node">XML node to check</param>
+        /// <param name="name">Name to check</param>
+        /// <returns>True if applying</returns>
+        internal static bool IsNode(XmlNode node, string name)
+        {
+            return node.LocalName.Equals(name, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        /// <summary>
+        /// Parses a float independent from the culture info of the host
+        /// </summary>
+        /// <param name="rawValue">Raw number as string</param>
+        /// <returns>Parsed float</returns>
+        internal static float ParseFloat(String rawValue)
+        {
+            return float.Parse(rawValue, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Parses an int independent from the culture info of the host
+        /// </summary>
+        /// <param name="rawValue">Raw number as string</param>
+        /// <returns>Parsed int</returns>
+        internal static int ParseInt(String rawValue)
+        {
+            return int.Parse(rawValue, CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Tries to parse an int independent from the culture info of the host
+        /// </summary>
+        /// <param name="rawvalue">Raw number as string</param>
+        /// <param name="parsedValue">Parsed int</param>
+        /// <returns>True, if the parsing was successful</returns>
+        internal static bool TryParseInt(String rawvalue, out int parsedValue)
+        {
+            return int.TryParse(rawvalue, NumberStyles.Integer, CultureInfo.InvariantCulture, out parsedValue);
+        }
+
+    }
+}
