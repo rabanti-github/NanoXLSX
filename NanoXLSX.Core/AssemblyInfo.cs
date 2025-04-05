5ca7c41ddcd4a6adf6668c9c9f277be54486169e
Raphael Stoeckli <github@rabanti.ch>
2025-04-05 21:37:02 +0200

    Solution cleanup

--- NanoXLSX.Core/AssemblyInfo.cs
+++ NanoXLSX.Core/AssemblyInfo.cs
@@ -1,8 +1,12 @@
 ﻿using System.Runtime.CompilerServices;
 
-
+// Core packages
 [assembly: InternalsVisibleTo("NanoXLSX.Writer")]
 [assembly: InternalsVisibleTo("NanoXLSX.Reader")]
-[assembly: InternalsVisibleTo("NanoXLSX.Formula")]
+// Test packages
 [assembly: InternalsVisibleTo("NanoXLSX.Core.Test")]
 [assembly: InternalsVisibleTo("NanoXLSX.Writer-Reader.Test")]
+// Plug-ins (backlog / reserved)
+[assembly: InternalsVisibleTo("NanoXLSX.Formula")]
+[assembly: InternalsVisibleTo("NanoXLSX.Security")]
+[assembly: InternalsVisibleTo("NanoXLSX.Formatting")]
